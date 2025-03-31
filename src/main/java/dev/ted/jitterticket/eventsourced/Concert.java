27cb1432b942533ab8cba11a7aca7fa1ceb0f7ce
Ted M. Young <tedyoung@gmail.com>
2025-03-31 14:01:35 -0700

    changed events field to be a mutable list

--- src/main/java/dev/ted/jitterticket/eventsourced/Concert.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/Concert.java
@@ -2,11 +2,12 @@ package dev.ted.jitterticket.eventsourced;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Concert {
 
-    private final List<ConcertEvent> concertEvents;
+    private final List<ConcertEvent> concertEvents = new ArrayList<>();
 
     public static Concert schedule(int price,
                                    LocalDateTime showDateTime,
@@ -20,7 +21,7 @@ public class Concert {
         ConcertScheduled concertScheduled = new ConcertScheduled(
                 price, showDateTime, doorsTime, capacity, maxTicketsPerPurchase
         );
-        concertEvents = List.of(concertScheduled);
+        concertEvents.add(concertScheduled);
     }
 
     public List<ConcertEvent> uncommittedEvents() {
