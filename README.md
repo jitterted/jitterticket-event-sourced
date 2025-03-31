b7f18a650220a5b62f80e793c1b41d261a416103
Ted M. Young <tedyoung@gmail.com>
2025-03-30 18:39:59 -0700

    Add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# JitterTicket: Event-Sourced Concert Ticketing
+
+Minimal (as possible) example of an event-sourced concert event ticketing application.
--- src/test/java/dev/ted/jitterticket/eventsourced/TestJitterTicketEventSourcedApplication.java
+++ src/test/java/dev/ted/jitterticket/eventsourced/TestJitterTicketEventSourcedApplication.java
@@ -2,7 +2,7 @@ package dev.ted.jitterticket.eventsourced;
 
 import org.springframework.boot.SpringApplication;
 
-public class TestJitterticketEventSourcedApplication {
+public class TestJitterTicketEventSourcedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.from(JitterTicketEventSourcedApplication::main).with(TestcontainersConfiguration.class).run(args);
