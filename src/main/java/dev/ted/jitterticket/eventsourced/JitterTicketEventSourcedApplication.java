8993ec833d5c40f0f5526f12a217df5a31892cc7
Ted M. Young <tedyoung@gmail.com>
2025-03-30 18:35:41 -0700

    Minor updates

--- src/main/java/dev/ted/jitterticket/eventsourced/JitterTicketEventSourcedApplication.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/JitterTicketEventSourcedApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JitterticketEventSourcedApplication {
+public class JitterTicketEventSourcedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JitterticketEventSourcedApplication.class, args);
+		SpringApplication.run(JitterTicketEventSourcedApplication.class, args);
 	}
 
 }
--- src/test/java/dev/ted/jitterticket/eventsourced/TestJitterticketEventSourcedApplication.java
+++ src/test/java/dev/ted/jitterticket/eventsourced/TestJitterticketEventSourcedApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestJitterticketEventSourcedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(JitterticketEventSourcedApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(JitterTicketEventSourcedApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
