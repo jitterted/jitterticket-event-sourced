478c26cc5b0836cbbd9e4451c416538a206e1462
Ted M. Young <tedyoung@gmail.com>
2025-04-01 12:45:23 -0700

    findAll works for one, though we're asserting using equals for Concert, which is not correct

--- src/test/java/dev/ted/jitterticket/eventsourced/application/ConcertStoreTest.java
+++ src/test/java/dev/ted/jitterticket/eventsourced/application/ConcertStoreTest.java
@@ -1,6 +1,10 @@
 package dev.ted.jitterticket.eventsourced.application;
 
+import dev.ted.jitterticket.eventsourced.domain.Concert;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -13,5 +17,20 @@ class ConcertStoreTest {
         assertThat(concertStore.findAll())
                 .as("There should be no concerts in a newly created ConcertStore")
                 .isEmpty();
+    }
+
+    @Test
+    void findAllReturnsOnlySavedConcert() {
+        ConcertStore concertStore = new ConcertStore();
+        Concert concert = Concert.schedule(99,
+                                           LocalDateTime.now(),
+                                           LocalTime.now().minusHours(1),
+                                           100,
+                                           4);
+
+        concertStore.save(concert);
+
+        assertThat(concertStore.findAll())
+                .containsExactly(concert);
     }
 }
