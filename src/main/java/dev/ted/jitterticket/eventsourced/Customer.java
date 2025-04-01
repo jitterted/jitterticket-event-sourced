c1c6b3a25b1b1130ef651aca7cf88c38ba84a05c
Ted M. Young <tedyoung@gmail.com>
2025-04-01 10:58:20 -0700

    Customer now generates the CustomerRegistered event upon instantiation

--- src/main/java/dev/ted/jitterticket/eventsourced/Customer.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/Customer.java
@@ -3,7 +3,11 @@ package dev.ted.jitterticket.eventsourced;
 public class Customer extends EventSourcedAggregate<CustomerEvent> {
 
     public static Customer register(String name, String email) {
-        return new Customer();
+        return new Customer(name, email);
+    }
+
+    public Customer(String name, String email) {
+        enqueue(new CustomerRegistered(name, email));
     }
 
     @Override
