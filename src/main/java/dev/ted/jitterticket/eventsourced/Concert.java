e2ebe8ff078b65af0126b065c1d04aaf789b907c
Ted M. Young <tedyoung@gmail.com>
2025-04-01 10:25:46 -0700

    Completed updating show/door times for ConcertRescheduled event

--- src/main/java/dev/ted/jitterticket/eventsourced/Concert.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/Concert.java
@@ -55,6 +55,7 @@ public class Concert {
                                     LocalTime newDoorsTime)
                     -> {
                 this.showDateTime = newShowDateTime;
+                this.doorsTime = newDoorsTime;
             }
         }
     }
