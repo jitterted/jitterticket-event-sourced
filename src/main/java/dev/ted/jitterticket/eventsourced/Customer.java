c2fc3f72cac2a312704fcafc777518c49e6f99b6
Ted M. Young <tedyoung@gmail.com>
2025-04-01 10:49:59 -0700

    Customer now extends EventSourcedAggregate, generified with the new CustomerEvent type

--- src/main/java/dev/ted/jitterticket/eventsourced/Customer.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/Customer.java
@@ -1,9 +1,13 @@
 package dev.ted.jitterticket.eventsourced;
 
-public class Customer {
+public class Customer extends EventSourcedAggregate<CustomerEvent> {
 
     public static Customer register(String name, String email) {
         return null;
     }
 
+    @Override
+    protected void apply(CustomerEvent customerEvent) {
+
+    }
 }
