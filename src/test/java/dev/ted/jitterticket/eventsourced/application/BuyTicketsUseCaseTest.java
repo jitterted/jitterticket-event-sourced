cb324571389c059e39295facea735c09e6b67338
Ted M. Young <tedyoung@gmail.com>
2025-04-01 12:23:45 -0700

    fails as expected

--- src/test/java/dev/ted/jitterticket/eventsourced/application/BuyTicketsUseCaseTest.java
+++ src/test/java/dev/ted/jitterticket/eventsourced/application/BuyTicketsUseCaseTest.java
@@ -3,6 +3,8 @@ package dev.ted.jitterticket.eventsourced.application;
 import dev.ted.jitterticket.eventsourced.domain.Concert;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
@@ -21,9 +23,16 @@ class BuyTicketsUseCaseTest {
 
     @Test
     void availableConcertsReturnsTheSingleTicketableConcert() {
-        // create Concert
-        // create BuyTicketsUseCase
-        // buyTicketsUseCase.availableConcerts()
-        // -> the Concert
+        Concert concert = Concert.schedule(99,
+                                           LocalDateTime.now(),
+                                           LocalTime.now().minusHours(1),
+                                           100,
+                                           4);
+        BuyTicketsUseCase buyTicketsUseCase = new BuyTicketsUseCase();
+
+        Stream<Concert> availableConcerts = buyTicketsUseCase.availableConcerts();
+
+        assertThat(availableConcerts)
+                .hasSize(1);
     }
 }
