82ac9718336504810aa4064f894b221223f6cdbb
Ted M. Young <tedyoung@gmail.com>
2025-04-09 13:27:36 -0700

    Refactor

--- src/main/java/dev/ted/jitterticket/eventsourced/adapter/in/web/BuyTicketController.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/adapter/in/web/BuyTicketController.java
@@ -19,10 +19,12 @@ import java.util.UUID;
 public class BuyTicketController {
 
     private final EventStore<ConcertId, ConcertEvent, Concert> concertStore;
+    private final BuyTicketsUseCase buyTicketsUseCase;
 
     @Autowired
     public BuyTicketController(EventStore<ConcertId, ConcertEvent, Concert> concertStore) {
         this.concertStore = concertStore;
+        buyTicketsUseCase = new BuyTicketsUseCase(concertStore);
     }
 
     @GetMapping("/concerts/{concertId}")
@@ -37,16 +39,16 @@ public class BuyTicketController {
     @PostMapping("/concerts/{concertId}")
     public String buyTickets(@PathVariable("concertId") String concertId,
                              TicketOrderForm ticketOrderForm) {
-        new BuyTicketsUseCase(concertStore)
-                .buyTickets(new ConcertId(UUID.fromString(concertId)),
-                            new CustomerId(UUID.fromString(ticketOrderForm.customerId())),
-                            ticketOrderForm.quantity());
+        buyTicketsUseCase.buyTickets(
+                new ConcertId(UUID.fromString(concertId)),
+                new CustomerId(UUID.fromString(ticketOrderForm.customerId())),
+                ticketOrderForm.quantity());
         return "redirect:/confirmations/af05fc05-2de1-46d8-9568-01381029feb7";
     }
 
     private ConcertView concertViewFor(String concertId) {
         return concertStore.findById(
-                new ConcertId(UUID.fromString(concertId)))
+                                   new ConcertId(UUID.fromString(concertId)))
                            .map(ConcertView::from)
                            .orElseThrow(() -> new RuntimeException("Could not find concert with id: " + concertId));
     }
