b47fb018a410a0b15c12afc5663e7ed3fd778fde
Ted M. Young <tedyoung@gmail.com>
2025-04-09 11:40:05 -0700

    Removed obsolete findAll from BuyTicketsUseCase

--- src/main/java/dev/ted/jitterticket/eventsourced/application/BuyTicketsUseCase.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/application/BuyTicketsUseCase.java
@@ -4,18 +4,12 @@ import dev.ted.jitterticket.eventsourced.domain.Concert;
 import dev.ted.jitterticket.eventsourced.domain.ConcertEvent;
 import dev.ted.jitterticket.eventsourced.domain.ConcertId;
 
-import java.util.stream.Stream;
-
 public class BuyTicketsUseCase {
 
     private final EventStore<ConcertId, ConcertEvent, Concert> concertStore;
 
     public BuyTicketsUseCase(EventStore<ConcertId, ConcertEvent, Concert> concertStore) {
         this.concertStore = concertStore;
-    }
-
-    public Stream<Concert> availableConcerts() {
-        return concertStore.findAll();
     }
 
 }
--- src/test/java/dev/ted/jitterticket/eventsourced/application/BuyTicketsUseCaseTest.java
+++ src/test/java/dev/ted/jitterticket/eventsourced/application/BuyTicketsUseCaseTest.java
@@ -1,46 +1,5 @@
 package dev.ted.jitterticket.eventsourced.application;
 
-import dev.ted.jitterticket.eventsourced.domain.Concert;
-import dev.ted.jitterticket.eventsourced.domain.ConcertEvent;
-import dev.ted.jitterticket.eventsourced.domain.ConcertId;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.UUID;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.*;
-
 class BuyTicketsUseCaseTest {
 
-    @Test
-    void availableConcertsReturnsNoConcertsWhenNoneCreated() {
-        BuyTicketsUseCase buyTicketsUseCase = new BuyTicketsUseCase(EventStore.forConcerts());
-
-        Stream<Concert> availableConcerts = buyTicketsUseCase.availableConcerts();
-
-        assertThat(availableConcerts)
-                .isEmpty();
-    }
-
-    @Test
-    void availableConcertsReturnsTheSingleTicketableConcert() {
-        Concert concert = Concert.schedule(new ConcertId(UUID.randomUUID()),
-                                           "Single",
-                                           99,
-                                           LocalDateTime.now(),
-                                           LocalTime.now().minusHours(1),
-                                           100,
-                                           4
-        );
-        EventStore<ConcertId, ConcertEvent, Concert> concertStore = EventStore.forConcerts();
-        concertStore.save(concert);
-        BuyTicketsUseCase buyTicketsUseCase = new BuyTicketsUseCase(concertStore);
-
-        Stream<Concert> availableConcerts = buyTicketsUseCase.availableConcerts();
-
-        assertThat(availableConcerts)
-                .hasSize(1);
-    }
 }
