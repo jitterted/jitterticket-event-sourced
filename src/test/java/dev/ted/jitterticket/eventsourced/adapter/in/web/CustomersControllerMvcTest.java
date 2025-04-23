2417cb794c12a282409390dad9e5fa8c1d77a415
Ted M. Young <tedyoung@gmail.com>
2025-04-23 11:41:38 -0700

    Put valid customer + concert into Stores for successful retrieval in the MVC-focused test

--- src/test/java/dev/ted/jitterticket/eventsourced/adapter/in/web/CustomersControllerMvcTest.java
+++ src/test/java/dev/ted/jitterticket/eventsourced/adapter/in/web/CustomersControllerMvcTest.java
@@ -1,6 +1,16 @@
 package dev.ted.jitterticket.eventsourced.adapter.in.web;
 
 import dev.ted.jitterticket.eventsourced.TixConfiguration;
+import dev.ted.jitterticket.eventsourced.application.EventStore;
+import dev.ted.jitterticket.eventsourced.domain.TicketOrderId;
+import dev.ted.jitterticket.eventsourced.domain.concert.Concert;
+import dev.ted.jitterticket.eventsourced.domain.concert.ConcertEvent;
+import dev.ted.jitterticket.eventsourced.domain.concert.ConcertFactory;
+import dev.ted.jitterticket.eventsourced.domain.concert.ConcertId;
+import dev.ted.jitterticket.eventsourced.domain.customer.Customer;
+import dev.ted.jitterticket.eventsourced.domain.customer.CustomerEvent;
+import dev.ted.jitterticket.eventsourced.domain.customer.CustomerFactory;
+import dev.ted.jitterticket.eventsourced.domain.customer.CustomerId;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +27,22 @@ class CustomersControllerMvcTest {
     @Autowired
     MockMvcTester mvc;
 
+    @Autowired
+    EventStore<ConcertId, ConcertEvent, Concert> concertStore;
+
+    @Autowired
+    EventStore<CustomerId, CustomerEvent, Customer> customerStore;
+
     @Test
     void getToCustomerTicketPurchaseConfirmationReturns200Ok() {
+        Concert concert = ConcertFactory.createConcert();
+        concertStore.save(concert);
+        Customer customer = CustomerFactory.newlyRegistered();
+        TicketOrderId ticketOrderId = TicketOrderId.createRandom();
+        customer.purchaseTickets(concert, ticketOrderId, 1);
+        customerStore.save(customer);
         mvc.get()
-           .uri("/customers/asdf/confirmations/qwerty")
+           .uri("/customers/" + customer.getId().id() + "/confirmations/" + ticketOrderId.id())
            .assertThat()
            .hasStatus2xxSuccessful();
     }
