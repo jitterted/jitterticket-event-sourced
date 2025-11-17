e9536a9aa13e5a8282ae06376e523bff5ca6149b
Ted M. Young <tedyoung@gmail.com>
2025-11-17 13:51:02 -0800

    Added (overloaded) constructor & field to store global event sequence

--- src/main/java/dev/ted/jitterticket/eventsourced/domain/Event.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/domain/Event.java
@@ -2,12 +2,22 @@ package dev.ted.jitterticket.eventsourced.domain;
 
 public abstract class Event {
     private final Integer eventSequence;
+    private final Long globalEventSequence;
 
     protected Event(Integer eventSequence) {
+        this(eventSequence, null);
+    }
+
+    protected Event(Integer eventSequence, Long globalEventSequence) {
         this.eventSequence = eventSequence;
+        this.globalEventSequence = globalEventSequence;
     }
 
     public Integer eventSequence() {
         return eventSequence;
+    }
+
+    public Long globalEventSequence() {
+        return globalEventSequence;
     }
 }
