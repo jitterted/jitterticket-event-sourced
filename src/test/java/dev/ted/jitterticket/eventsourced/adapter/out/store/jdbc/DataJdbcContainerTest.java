0bf17baec136b015dd0a5493677b0f85ee3492fa
Ted M. Young <tedyoung@gmail.com>
2025-11-20 12:22:55 -0800

    Improved the database base test class to use reusable containers and truncate the data before the test class runs

--- src/test/java/dev/ted/jitterticket/eventsourced/adapter/out/store/jdbc/DataJdbcContainerTest.java
+++ src/test/java/dev/ted/jitterticket/eventsourced/adapter/out/store/jdbc/DataJdbcContainerTest.java
@@ -1,21 +1,47 @@
 package dev.ted.jitterticket.eventsourced.adapter.out.store.jdbc;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Tag("spring")
+@Tag("db")
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
-public class DataJdbcContainerTest {
+public abstract class DataJdbcContainerTest {
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.6");
+    static PostgreSQLContainer<?> postgres;
 
+    static {
+        postgres = new PostgreSQLContainer<>("postgres:17.6")
+                .withReuse(true);
+        // manually start instead of using @Container
+        postgres.start();
+    }
+
+    // Manually register instead of using ServiceConnection
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeAll
+    public static void truncateAll(@Autowired JdbcTemplate jdbcTemplate) {
+        jdbcTemplate.execute("""
+            DO $$ DECLARE
+                r RECORD;
+            BEGIN
+                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+                    EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE';
+                END LOOP;
+            END $$;
+        """);
+    }
 }
