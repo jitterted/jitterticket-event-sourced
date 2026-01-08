fb3401f243aa12856e01d7fb5747e0df502998e2
Ted M. Young <tedyoung@gmail.com>
2026-01-08 13:08:55 -0800

    Removed obsolete code from RegisteredCustomersProjector, leaving the pure domain projection code

--- src/main/java/dev/ted/jitterticket/eventsourced/application/RegisteredCustomersProjector.java
+++ src/main/java/dev/ted/jitterticket/eventsourced/application/RegisteredCustomersProjector.java
@@ -1,10 +1,7 @@
 package dev.ted.jitterticket.eventsourced.application;
 
 import dev.ted.jitterticket.Gatherers;
-import dev.ted.jitterticket.eventsourced.application.port.EventStore;
-import dev.ted.jitterticket.eventsourced.domain.customer.Customer;
 import dev.ted.jitterticket.eventsourced.domain.customer.CustomerEvent;
-import dev.ted.jitterticket.eventsourced.domain.customer.CustomerId;
 import dev.ted.jitterticket.eventsourced.domain.customer.CustomerRegistered;
 
 import java.util.List;
@@ -12,32 +9,6 @@ import java.util.stream.Stream;
 
 public class RegisteredCustomersProjector implements
         DomainProjector<CustomerEvent, RegisteredCustomers> {
-    private final EventStore<CustomerId, CustomerEvent, Customer> customerStore;
-    private final RegisteredCustomers registeredCustomers = new RegisteredCustomers();
-
-    public RegisteredCustomersProjector(EventStore<CustomerId, CustomerEvent, Customer> customerStore) {
-        this.customerStore = customerStore;
-        Stream<CustomerEvent> allCustomerEvents = this.customerStore
-                .allEventsAfter(0L);
-        registeredCustomers.add(registeredCustomers(allCustomerEvents));
-    }
-
-    public RegisteredCustomersProjector() {
-        customerStore = null;
-    }
-
-    public RegisteredCustomers allCustomers() {
-        return registeredCustomers;
-    }
-
-    private List<RegisteredCustomers.RegisteredCustomer> registeredCustomers(Stream<CustomerEvent> allCustomerEvents) {
-        return allCustomerEvents
-                .gather(Gatherers.filterAndCastTo(CustomerRegistered.class))
-                .map(registered -> new RegisteredCustomers.RegisteredCustomer(
-                        registered.customerId(),
-                        registered.customerName()))
-                .toList();
-    }
 
     @Override
     public ProjectorResult<RegisteredCustomers> project(
