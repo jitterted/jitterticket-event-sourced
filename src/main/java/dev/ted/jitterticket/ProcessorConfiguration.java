a3ff1eb4ad85c4ed9799d343d10fe0ab7d4a7f3e
Ted M. Young <tedyoung@gmail.com>
2026-04-07 14:46:09 -0700

    Manually specified the desired events for the ConcertStartedProcessor so I can stop using the (now) deprecated subscribe() method.

--- src/main/java/dev/ted/jitterticket/ProcessorConfiguration.java
+++ src/main/java/dev/ted/jitterticket/ProcessorConfiguration.java
@@ -4,15 +4,20 @@ import dev.ted.jitterticket.eventsourced.application.Checkpoint;
 import dev.ted.jitterticket.eventsourced.application.CommandExecutorFactory;
 import dev.ted.jitterticket.eventsourced.application.ConcertStartedProcessor;
 import dev.ted.jitterticket.eventsourced.application.port.EventStore;
+import dev.ted.jitterticket.eventsourced.domain.Event;
 import dev.ted.jitterticket.eventsourced.domain.concert.Concert;
 import dev.ted.jitterticket.eventsourced.domain.concert.ConcertEvent;
 import dev.ted.jitterticket.eventsourced.domain.concert.ConcertId;
+import dev.ted.jitterticket.eventsourced.domain.concert.ConcertRescheduled;
+import dev.ted.jitterticket.eventsourced.domain.concert.ConcertScheduled;
+import dev.ted.jitterticket.eventsourced.domain.concert.TicketSalesStopped;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Stream;
 
@@ -31,11 +36,14 @@ public class ProcessorConfiguration {
                         Clock.systemDefaultZone(),
                         CommandExecutorFactory.create(concertStore));
         log.info("Concert Started Processor: catching up on all events");
+        Set<Class<? extends Event>> desiredEvents = Set.of(ConcertScheduled.class,
+                                                           ConcertRescheduled.class,
+                                                           TicketSalesStopped.class);
         Stream<ConcertEvent> catchUpEventStream =
-                concertStore.allEventsAfter(Checkpoint.INITIAL);
+                concertStore.allEventsAfter(Checkpoint.INITIAL, desiredEvents);
         log.info("Handling the events...");
         concertStartedProcessor.handle(catchUpEventStream);
-        concertStore.subscribe(concertStartedProcessor);
+        concertStore.subscribe(concertStartedProcessor, desiredEvents);
         return concertStartedProcessor;
     }
 }
